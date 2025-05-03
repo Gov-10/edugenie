6df79475274bfa4140c5e1b8c29441fb9d7d2b3b
Govind <govindsys1008@gmail.com>
2025-05-03 13:49:39 +0530

    implemented password hashing and redirection (to be changed later)

--- edugenie_proj/myedu/forms.py
+++ edugenie_proj/myedu/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Student
 
 class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = Student
         fields = [
@@ -10,3 +11,8 @@ class SignupForm(forms.ModelForm):
             'password', 
             'school_student'
         ]
+
+class SigninForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput)
+
--- edugenie_proj/myedu/views.py
+++ edugenie_proj/myedu/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import SignupForm
+from .forms import SignupForm, SigninForm
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -9,4 +9,6 @@ def signup(request):
     form = SignupForm(request.POST or None)
     if form.is_valid():
         form.save()
-    return render(request, 'sign_up.html', {'form': form})
+        return render(request, 'home.html', {'form': form, 'success': True})
+    else:
+        return render(request, 'sign_up.html', {'form': form})
