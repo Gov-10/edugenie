64ac1085d7c3d8fa7f81b8053288db81557267b1
Govind <govindsys1008@gmail.com>
2025-05-05 15:57:24 +0530

    adding styling to the form

--- edugenie_proj/myedu/forms.py
+++ edugenie_proj/myedu/forms.py
@@ -3,6 +3,7 @@ from .models import Student
 
 class SignupForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    school_student = forms.BooleanField(label="Are you a school student?")
     class Meta:
         model = Student
         fields = [
