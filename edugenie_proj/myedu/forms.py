6e35265431623a2659f7bf8e95c9c3a813481b83
Govind <govindsys1008@gmail.com>
2025-05-16 19:05:29 +0530

    implemented password hashing

--- edugenie_proj/myedu/forms.py
+++ edugenie_proj/myedu/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from .models import Student
+from django.contrib.auth.hashers import make_password
 
 class SignupForm(forms.ModelForm):
-    password = forms.CharField(max_length=10,widget=forms.PasswordInput)
+    password = forms.CharField(max_length=128,widget=forms.PasswordInput)
     school_student = forms.BooleanField(required=False,label="Are you a school student?")
     class Meta:
         model = Student
@@ -12,9 +13,17 @@ class SignupForm(forms.ModelForm):
             'password', 
             'school_student'
         ]
+    def save(self, commit=True):
+        student = super().save(commit=False)
+        student.password = make_password(self.cleaned_data['password'])
+        if commit:
+            student.save()
+        return True
+        
 
 class SigninForm(forms.Form):
     name = forms.CharField(max_length=100, required=True)
     password = forms.CharField(required=True, widget=forms.PasswordInput)
     stud = forms.BooleanField(required=False, label= "Are you a school student?")
+
 
